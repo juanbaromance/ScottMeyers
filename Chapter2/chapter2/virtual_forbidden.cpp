a17b8ba93f8688eccc99ffa31130cc746858cb34
juanba <juanba.romance@gmail.com>
2019-11-02 01:22:33 +0100

    Item 9/10 + metaprograming stuff

--- Chapter2/chapter2/virtual_forbidden.cpp
+++ Chapter2/chapter2/virtual_forbidden.cpp
@@ -1,50 +1,56 @@
+// item 9 Proof on virtual call undefined behaviour when requested on the construction phase
+// item 10 Application on monadics smelling returning self rererences, same as operator = , copy constructor t
+// extra stuff: templates acting as interfaces through static polymorphism, sohemow a crtp
+
 #include <memory>
 #include <boost/format.hpp>
 #include <iostream>
 
+template <typename T1, typename T2>
 class iA {
 public:
-    iA()
-    {
-        startup();
-    }
+    iA(){ startup(); }
 
-    virtual void dump() = 0;
-    virtual ~iA() = 0;
+    virtual T1& dump() = 0;
+    virtual ~iA(){};
+
+    /* Proper parametrization */
+    virtual T1& x( const T2 & tmp ) = 0;
+    virtual T1& y( const T2 & tmp ) = 0;
 
 private:
-    void startup(){
-        //    dump();
+    void startup()
+    {
+        // uncoment below to die
+        // dump();
     }
 };
 
-iA::~iA(){}
 
-class A : public iA {
+// derivated class and customization are both injected as a Template params
+// into the interface
+
+template<typename T=int>
+class A : public iA< A<T>, T > {
 public:
     // Error prone not... even more
-    A( int p1 = 0, int p2 = 0 ) : _p1(p1), _p2(p2){}
+    A( T x = 0, T y = 0 ) : _x(x), _y(y){}
 
     /* Proper parametrization */
-    A& p1( int tmp ){ _p1 = tmp; return *this; }
-    A& p2( int tmp ){ _p2 = tmp; return *this; }
-
-    void dump() override
+    A<T>& x( const T & tmp )override { _x = tmp; return *this; }
+    A<T>& y( const T & tmp )override { _y = tmp; return *this; }
+    A<T>& dump() override
     {
-        ( std::cout << boost::format("%s(%03d,%03d)(%p)\n") % __PRETTY_FUNCTION__ % _p1 % _p2 % this ).flush();
+        ( std::cout << boost::format("%s(%03d,%03d)(%p)\n") % __PRETTY_FUNCTION__ % _x % _y % this ).flush();
+        return *this;
     }
 
 private:
-    int _p1,_p2;
+    T _x, _y;
 };
 
 int main()
 {
-    { std::shared_ptr<iA> a = std::make_shared<A>(0,-1); }
-
-    A a;
-    a.dump();
-
-    a.p1( -3 ).p2( -1 );
-    a.dump();
+    std::shared_ptr<iA<A<>,int>> a = std::make_shared<A<>>(0,-1);
+    a->x( -3 ).dump().y( -5 ).dump();
 }
